ae2d6bf26c04b083051b4128f023ac33260072ba
migue <migue@LAPTOP-GADGJENV>
2017-06-05 10:40:10 +0100

    new stuff

--- ProjetoTS-Cliente/Form1.Designer.cs
+++ ProjetoTS-Cliente/Form1.Designer.cs
@@ -29,33 +29,118 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 50);
+            this.button1.Location = new System.Drawing.Point(338, 67);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.Size = new System.Drawing.Size(168, 73);
             this.button1.TabIndex = 0;
             this.button1.Text = "Receber Chave Pública";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(140, 156);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(30, 156);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(75, 23);
+            this.buttonRegister.TabIndex = 35;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 24);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Login Window";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(30, 120);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(185, 20);
+            this.textBoxPassword.TabIndex = 33;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(30, 72);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(185, 20);
+            this.textBoxUsername.TabIndex = 32;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Username";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 136);
+            this.ClientSize = new System.Drawing.Size(531, 198);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ProjetoTS-Cliente/Form1.cs
+++ ProjetoTS-Cliente/Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,6 +22,7 @@ namespace ProjetoTS_Cliente
         private ProtocolSI protocolsi;
         private RSACryptoServiceProvider rsa;
         private const int PORT = 9999;
+        private const int SALTSIZE = 8;
 
         public Form1()
         {
@@ -96,6 +99,206 @@ namespace ProjetoTS_Cliente
 
             //Console.WriteLine("Server");
             //Console.ReadKey();
+        }
+
+        private bool VerifyLogin(string username, string password)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                // Configurar ligação à Base de Dados
+                conn = new SqlConnection();
+                conn.ConnectionString = String.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\migue\Documents\ESTG\Tópicos de Segurança\FichaPratica8_Base\FichaPratica8_Base\Database1.mdf';Integrated Security=True");
+
+                // Abrir ligação à Base de Dados
+                conn.Open();
+
+                // Declaração do comando SQL
+                String sql = "SELECT * FROM Users WHERE Username = @username";
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = sql;
+
+                // Declaração dos parâmetros do comando SQL
+                SqlParameter param = new SqlParameter("@username", username);
+
+                // Introduzir valor ao parâmentro registado no comando SQL
+                cmd.Parameters.Add(param);
+
+                // Associar ligação à Base de Dados ao comando a ser executado
+                cmd.Connection = conn;
+
+                // Executar comando SQL
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    throw new Exception("Error while trying to access an user");
+                }
+
+                // Ler resultado da pesquisa
+                reader.Read();
+
+                // Obter Hash (password + salt)
+                byte[] saltedPasswordHashStored = (byte[])reader["SaltedPasswordHash"];
+
+                // Obter salt
+                byte[] saltStored = (byte[])reader["Salt"];
+
+                conn.Close();
+
+                byte[] pass = Encoding.UTF8.GetBytes(password);
+
+                byte[] hash = GenerateSaltedHash(pass, saltStored);
+
+                return saltedPasswordHashStored.SequenceEqual(hash);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("An error occurred");
+                throw e;
+            }
+        }
+
+        private void Register(string username, byte[] saltedPasswordHash, byte[] salt)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                // Configurar ligação à Base de Dados
+                conn = new SqlConnection();
+                conn.ConnectionString = String.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\migue\Documents\ESTG\Tópicos de Segurança\FichaPratica8_Base\FichaPratica8_Base\Database1.mdf';Integrated Security=True");
+
+                // Abrir ligação à Base de Dados
+                conn.Open();
+
+                // Declaração dos parâmetros do comando SQL
+                SqlParameter paramUsername = new SqlParameter("@username", username);
+                SqlParameter paramPassHash = new SqlParameter("@saltedPasswordHash", saltedPasswordHash);
+                SqlParameter paramSalt = new SqlParameter("@salt", salt);
+
+                // Declaração do comando SQL
+                String sql = "INSERT INTO Users (Username, SaltedPasswordHash, Salt) VALUES (@username,@saltedPasswordHash,@salt)";
+
+                // Prepara comando SQL para ser executado na Base de Dados
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                // Introduzir valores aos parâmentros registados no comando SQL
+                cmd.Parameters.Add(paramUsername);
+                cmd.Parameters.Add(paramPassHash);
+                cmd.Parameters.Add(paramSalt);
+
+                // Executar comando SQL
+                int lines = cmd.ExecuteNonQuery();
+
+                // Fechar ligação
+                conn.Close();
+                if (lines == 0)
+                {
+                    // Se forem devolvidas 0 linhas alteradas então o não foi executado com sucesso
+                    throw new Exception("Error while inserting an user");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error while inserting an user:" + e.Message);
+            }
+        }
+
+        private static byte[] GenerateSalt(int size)
+        {
+            //Generate a cryptographic random number.
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[size];
+            rng.GetBytes(buff);
+            return buff;
+        }
+
+        private static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            using (HashAlgorithm hashAlgorithm = SHA512.Create())
+            {
+                // Declarar e inicializar buffer para o texto e salt
+                byte[] plainTextWithSaltBytes =
+                              new byte[plainText.Length + salt.Length];
+
+                // Copiar texto para buffer
+                for (int i = 0; i < plainText.Length; i++)
+                {
+                    plainTextWithSaltBytes[i] = plainText[i];
+                }
+                // Copiar salt para buffer a seguir ao texto
+                for (int i = 0; i < salt.Length; i++)
+                {
+                    plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+                }
+
+                //Devolver hash do text + salt
+                return hashAlgorithm.ComputeHash(plainTextWithSaltBytes);
+            }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            String pass = textBoxPassword.Text;
+
+            String username = textBoxUsername.Text;
+
+            byte[] salt = GenerateSalt(SALTSIZE);
+
+            byte[] hash = GenerateSaltedHash(Encoding.UTF8.GetBytes(pass), salt);
+
+            Register(username, hash, salt);
+
+            ClientThread ct = new ClientThread("Thread 1");
+            ct.run();
+
+            ct = new ClientThread("Thread 2");
+            ct.run();
+        }
+
+        private class ClientThread
+        {
+            private String message;
+
+            public ClientThread(String message)
+            {
+                this.message = message;
+            }
+
+            public void run()
+            {
+                Thread t = new Thread(handleClient);
+                t.Start();
+            }
+
+            private void handleClient()
+            {
+                MessageBox.Show(message);
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            String pass = textBoxPassword.Text;
+
+            byte[] salt = GenerateSalt(SALTSIZE);
+
+            byte[] hash = GenerateSaltedHash(Encoding.UTF8.GetBytes(pass), salt);
+
+            //textBoxSaltedHash.Text = Convert.ToBase64String(hash);
+
+            //textBoxSalt.Text = Convert.ToBase64String(salt);
+
+            String username = textBoxUsername.Text;
+
+            if (VerifyLogin(username, pass))
+            {
+                MessageBox.Show("Valid User");
+            }
+            else
+            {
+                MessageBox.Show("Invalid User");
+            }
         }
 
     }
