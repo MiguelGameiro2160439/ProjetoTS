cbf784899da311e374de24938bc4cb2ac90f93f1
João Vieira <Joo Vieira@DESKTOP-V1NPST5>
2017-06-05 20:52:49 +0100

    testa esta merda

--- ProjetoTS-Cliente/Form1.Designer.cs
+++ ProjetoTS-Cliente/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.buttonRegister = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
@@ -37,16 +36,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(338, 67);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Receber Chave Pública";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // buttonLogin
             // 
@@ -115,7 +104,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 198);
+            this.ClientSize = new System.Drawing.Size(240, 198);
             this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.buttonRegister);
             this.Controls.Add(this.label3);
@@ -123,7 +112,6 @@
             this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -132,8 +120,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button buttonLogin;
         private System.Windows.Forms.Button buttonRegister;
         private System.Windows.Forms.Label label3;
--- ProjetoTS-Cliente/FormFicheiros.cs
+++ ProjetoTS-Cliente/FormFicheiros.cs
@@ -1,21 +1,33 @@
-﻿using System;
+﻿using EI.SI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace ProjetoTS_Cliente
 {
     public partial class FormFicheiros : Form
     {
+        private ProtocolSI protocolsi;
+        private RSACryptoServiceProvider rsa;
+        private const int PORT = 9999;
+        private const int SALTSIZE = 8;
         public FormFicheiros()
         {
             InitializeComponent();
+            rsa = new RSACryptoServiceProvider();
+            TcpClient tcpClient = new TcpClient();
+            protocolsi = new ProtocolSI();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,8 +36,8 @@ namespace ProjetoTS_Cliente
             int bytesRead = 0;
             byte[] buffer = new byte[bufferSize];
 
-            String originalFilePath = "security.jpg";
-            String copyfilePath = "copy_security.jpg";
+            String originalFilePath = "estg_logo.jpg";
+            String copyfilePath = "copy_estg_logo.jpg";
             String logFilePath = "log.txt";
 
             if (File.Exists(copyfilePath))
@@ -53,5 +65,77 @@ namespace ProjetoTS_Cliente
             originalFileStream.Close();
             copyFileStream.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rsa = new RSACryptoServiceProvider();
+            TcpClient tcpClient = new TcpClient();
+            NetworkStream networkStream = null;
+
+            try
+            {
+                tcpClient = new TcpClient();
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, PORT);
+
+                //Console.WriteLine("Start connection to server? press any key");
+                //Console.ReadKey();
+
+                tcpClient.Connect(endPoint);
+                networkStream = tcpClient.GetStream();
+
+                int bytesRead = 0;
+
+                networkStream.Read(protocolsi.Buffer, 0, protocolsi.Buffer.Length);
+
+                if (protocolsi.GetCmdType() == ProtocolSICmdType.PUBLIC_KEY)
+                {
+                    MessageBox.Show("Recebi: " + protocolsi.GetStringFromData());
+                }
+
+                #region Send String Message
+
+                // Console.WriteLine("Send message to server? write a message");
+                string msg = Console.ReadLine();
+
+                byte[] msgBytes = Encoding.UTF8.GetBytes(msg);
+                networkStream.Write(msgBytes, 0, msgBytes.Length);
+
+                // Receive ack
+
+                byte[] ack = new byte[2];
+
+                bytesRead = networkStream.Read(ack, 0, ack.Length);
+
+                rsa.FromXmlString("publickey");
+
+                string ackMessage = Encoding.UTF8.GetString(ack, 0, bytesRead);
+
+                //Console.WriteLine("Received:" + ackMessage);
+
+                #endregion
+            }
+            catch (Exception exception)
+            {
+                //Console.WriteLine(exception.Message);
+            }
+
+            finally
+            {
+                if (networkStream != null)
+                {
+                    networkStream.Close();
+                }
+
+                if (tcpClient != null)
+                {
+                    tcpClient.Close();
+                }
+            }
+
+            //Console.WriteLine("Server");
+            //Console.ReadKey();
+        }
+
     }
+    
 }
